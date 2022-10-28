80a455417ecea79811cdba868ee9949e85c0c68d
Vladislav Borisov <93284707+BugsDestroyer@users.noreply.github.com>
2022-10-28 18:22:52 +0300

    Add files via upload

---
+++ BattleShip.js
@@ -0,0 +1,50 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7];
+var rand = Math.floor(Math.random() * numbers.length);
+var pos_1 = rand;
+var pos_2 = pos_1 +1 ;
+var pos_3 = pos_2 + 1;
+var a = [];
+
+var shots = 0;
+var hits = 0;
+var TryToHit;
+var VinMessege = " попытки"
+
+var IsSunk = false;
+
+
+while(IsSunk == false) {
+	TryToHit = prompt("Попытй удачу! Попробуй попасть по кораблю! Введите цифру от 0 до 9!");
+	if(TryToHit < 0 || TryToHit > 9 || TryToHit == null){
+		alert("Видимо мы друг друга не поняли! Введите цифру от 0 до 9!");
+	}else{
+		shots += 1;
+		if(a.indexOf(TryToHit) !== -1 ){
+			shots -= 1
+			alert("Ты уже попал в эту часть коробля! Попробуй попасть в другие!");
+		}else{
+			if (TryToHit == pos_1 || TryToHit == pos_2 || TryToHit == pos_3) {
+				a.push(TryToHit);
+				hits += 1;
+				alert("Есть поподание! Продолжай в том же духе!");
+			}else{
+				alert("Мимо! Попробуй еще раз!");
+			}
+			if(hits == 3){
+					IsSunk == true;
+					if(shots > 4){
+						VinMessege = " попыток";
+					}
+					alert("Победа! Корабль потоплен за " + shots + VinMessege +" ! Хочешь сыграть еще раз?");
+					shots = 0;
+					hits = 0;
+					a = [];
+					rand = Math.floor(Math.random() * numbers.length);
+					pos_1 = rand;
+					pos_2 = pos_1 +1 ;
+					pos_3 = pos_2 + 1;
+			}
+		}
+	}
+}
+
