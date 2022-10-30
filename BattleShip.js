d758f17e0c75ef241d313783576366672c2e8895
Vladislav Borisov <93284707+BugsDestroyer@users.noreply.github.com>
2022-10-30 14:30:34 +0300

    Update BattleShip.js

--- BattleShip.js
+++ BattleShip.js
@@ -9,41 +9,53 @@ var shots = 0;
 var hits = 0;
 var TryToHit;
 var VinMessege = " попытки"
-
+var RestartGame;
 var IsSunk = false;
 
 
 while(IsSunk == false) {
-	TryToHit = prompt("Попытй удачу! Попробуй попасть по кораблю! Введите цифру от 0 до 9!");
-	if(TryToHit < 0 || TryToHit > 9 || isNaN(TryToHit) || TryToHit == null){
-		alert("Видимо мы друг друга не поняли! Введите цифру от 0 до 9!");
+	TryToHit = prompt("Попытай удачу! Попробуй попасть по кораблю! Введите цифру от 1 до 9!");
+	if(TryToHit == null){
+		alert("Пока! Буду ждать твоего возвращения :)");
+		IsSunk == true;
+        Window.close();
+	}
+	if(TryToHit < 1 || TryToHit > 9 || isNaN(TryToHit) || TryToHit == ''){
+		alert("Видимо мы друг друга не поняли! Введите цифру от 1 до 9!");
 	}else{
 		shots += 1;
 		if(a.indexOf(TryToHit) !== -1 ){
 			shots -= 1
-			alert("Ты уже попал в эту часть коробля! Попробуй попасть в другие!");
+			alert("Ты уже попал в эту часть корабля! Попробуй попасть в другие!");
 		}else{
 			if (TryToHit == pos_1 || TryToHit == pos_2 || TryToHit == pos_3) {
 				a.push(TryToHit);
 				hits += 1;
-				alert("Есть поподание! Продолжай в том же духе!");
+				alert("Есть попадание! Продолжай в том же духе!");
 			}else{
 				alert("Мимо! Попробуй еще раз!");
 			}
-			if(hits == 3){
-				IsSunk == true;
-					if(shots > 4){
-						VinMessege = " попыток";
-					}
-				alert("Победа! Корабль потоплен за " + shots + VinMessege +" ! Хочешь сыграть еще раз?");
-				shots = 0;
-				hits = 0;
-				a = [];
-				rand = Math.floor(Math.random() * numbers.length);
-				pos_1 = numbers[rand];
-				pos_2 = pos_1 +1 ;
-				pos_3 = pos_2 + 1;
-			}
+				if(hits == 3){
+						IsSunk == true;
+						if(shots > 4){
+							VinMessege = " попыток";
+						}
+							if (window.confirm("Победа! Корабль потоплен за " + shots + VinMessege +" ! Хочешь сыграть еще раз?")){
+								shots = 0;
+								hits = 0;
+								a = [];
+								rand = Math.floor(Math.random() * numbers.length);
+								pos_1 = numbers[rand];
+								pos_2 = pos_1 +1 ;
+								pos_3 = pos_2 + 1;
+								IsSunk == false;
+						 	}else{
+								alert("Пока! Буду ждать твоего возвращения :)");
+								Window.close();
+							}
+							
+				}
 		}
 	}
 }
+
